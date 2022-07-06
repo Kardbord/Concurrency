102c38f6eac79f7c343a4295f7fa68971f336d09
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2022-07-05 18:56:48 -0600

    Updated DefaultConstructor test

--- tests/ShardedUnorderedConcurrentMapTests.cpp
+++ tests/ShardedUnorderedConcurrentMapTests.cpp
@@ -20,12 +20,33 @@ TYPED_TEST_P(ShardedUnorderedConcurrentMapTest, DefaultConstructor) {
   constexpr typename map_type::size_type expected_size = 0;
   ASSERT_EQ(expected_size, umap.size());
   ASSERT_TRUE(umap.empty());
+
   try {
     umap.at(TypeParam());
+    FAIL() << "Expected std::out_of_range.";
   } catch (std::out_of_range const &) {
   } catch (...) {
     FAIL() << "Expected std::out_of_range.";
   }
+
+  try {
+    umap[TypeParam()];
+    FAIL() << "Expected std::out_of_range.";
+  } catch (std::out_of_range const &) {
+  } catch (...) {
+    FAIL() << "Expected std::out_of_range.";
+  }
+
+  EXPECT_EQ(0, umap.erase(TypeParam())) << "Expected no elements to be removed.";
+  EXPECT_EQ(0, umap.count(TypeParam())) << "Expected no elements to be found.";
+  EXPECT_FALSE(umap.find(TypeParam()));
+
+  auto data = umap.data();
+  ASSERT_EQ(expected_size, data.size());
+  ASSERT_TRUE(data.empty());
+
+  EXPECT_EQ(map_type().data(), data);
+  EXPECT_FALSE(data != map_type().data());
 }
 
 REGISTER_TYPED_TEST_SUITE_P(ShardedUnorderedConcurrentMapTest, DefaultConstructor);
