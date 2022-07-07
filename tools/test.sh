4d50d89b8536820db637270827222cedc111c693
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2022-07-07 15:47:10 -0600

    Added code coverage

--- tools/test.sh
+++ tools/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-pushd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null
+pushd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null
 ./build.sh
 ../build/ConcurrentMap_test
-popd > /dev/null
+popd >/dev/null
