60c87180643cab7a8598ae05d94881677739fbf1
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2022-07-19 00:05:20 -0600

    Fixed install.sh bug where default install location was not respected

--- tools/install.sh
+++ tools/install.sh
@@ -15,30 +15,34 @@ function usage() {
 INSTALL_PREFIX=""
 while getopts ":hp:" opt; do
   case "${opt}" in
-  p)
-    INSTALL_PREFIX="${OPTARG}"
-    ;;
-  h)
-    usage
-    exit 0
-    ;;
-  *)
-    usage
-    exit 1
-    ;;
+    p)
+      INSTALL_PREFIX="${OPTARG}"
+      ;;
+    h)
+      usage
+      exit 0
+      ;;
+    *)
+      usage
+      exit 1
+      ;;
   esac
 done
 shift $((OPTIND - 1))
 
-if [[ -n "${INSTALL_PREFIX}" ]] && ! [[ -d "${INSTALL_PREFIX}" ]]; then
-  echo "No such directory: ${INSTALL_PREFIX}"
-  exit 1
-fi
-
 mkdir -p "$(dirname "${BASH_SOURCE[0]}")/../build"
 pushd "$(dirname "${BASH_SOURCE[0]}")/../build" >/dev/null
 rm -rf ./* # Start from clean slate
-cmake .. "-DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_PREFIX}" -DINSTALL=ON
+if [[ -n "${INSTALL_PREFIX}" ]]; then
+  if ! [[ -d "${INSTALL_PREFIX}" ]]; then
+    echo "No such directory: ${INSTALL_PREFIX}"
+    exit 1
+  fi
+  cmake .. "-DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_PREFIX}" -DINSTALL=ON
+else
+  cmake .. -DINSTALL=ON
+fi
 cmake --build . --config Release --target install -- -j "$(nproc)"
+
 rm -rf ./* # Clean up build files
 popd >/dev/null
